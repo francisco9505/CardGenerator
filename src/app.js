b4b7e754337f79c8c96bf8a6b3805d8d4bd366fc
francisco9505 <franciscocastillo9505@gmail.com>
2023-09-07 15:16:16 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const cambiarNumeroYSimbolo = () => {
+  let card = document.querySelector(".card");
+
+  let numerosYLetras = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indice = Math.floor(Math.random() * numerosYLetras.length);
+
+  let nuevoNumero = numerosYLetras[indice];
+
+  let numeroTarjeta = card.querySelector(".number");
+  numeroTarjeta.textContent = nuevoNumero;
+
+  let simbolos = ["♦", "♥", "♠", "♣"];
+
+  let nuevoSimbolo = simbolos[Math.floor(Math.random() * simbolos.length)];
+
+  let arriba = card.querySelector(".top");
+  let abajo = card.querySelector(".bottom");
+
+  arriba.textContent = nuevoSimbolo;
+  abajo.textContent = nuevoSimbolo;
+
+  if (nuevoSimbolo === "♦" || nuevoSimbolo === "♥") {
+    arriba.style.color = "red";
+    abajo.style.color = "red";
+  } else {
+    arriba.style.color = "black";
+    abajo.style.color = "black";
+  }
 };
+
+window.onload = cambiarNumeroYSimbolo;
